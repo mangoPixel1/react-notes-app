b8b4427f3065dc63f0144acb4d7ec7947152bab6
Miguel Cavada <70104762+mangoPixel1@users.noreply.github.com>
2026-01-09 15:32:24 -0700

    Enhance Header and Sidebar UI with new icons
    Added CircleUserRound icon and improved button styling in Header. Sidebar now features additional navigation icons, dark mode support, and updated layout for better usability.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,7 @@ import Search from "./Search";
 
 // Icons
 import Logo from "../icons/Logo";
-import { Sun } from "lucide-react";
-import { Moon } from "lucide-react";
-import { RefreshCw } from "lucide-react";
+import { Sun, Moon, RefreshCw, CircleUserRound } from "lucide-react";
 
 function Header() {
   const location = useLocation();
@@ -47,7 +45,7 @@ function Header() {
           <Search />
         </div>
 
-        <div className="space-x-2">
+        <div className="flex gap-2">
           {isHome && (
             <>
               <button
@@ -68,6 +66,9 @@ function Header() {
             ) : (
               <Moon className="w-6 h-6 text-amber-500" />
             )}
+          </button>
+          <button className="px-2 py-2 rounded-4xl cursor-pointer hover:bg-gray-50 dark:hover:bg-orange-950 transition duration-300">
+            <CircleUserRound className="w-8 h-8 text-gray-500 dark:text-gray-400" />
           </button>
         </div>
       </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,42 @@
+import { useContext } from "react";
+import { UIContext } from "../contexts/UIContext";
+
 // Icons
-import { Menu } from "lucide-react";
+import {
+  Menu,
+  House,
+  FolderClosed,
+  Archive,
+  Trash2,
+  Settings,
+} from "lucide-react";
 
 function Sidebar() {
+  const { isDark } = useContext(UIContext);
+
   return (
-    <aside className="w-20 shrink-0 bg-orange-200 p-4 flex flex-col items-center gap-6">
-      <Menu className="w-8 h-8 text-amber-700" />
+    <aside className="w-16 shrink-0 bg-orange-200 dark:bg-amber-900 p-4 flex flex-col items-center gap-6">
+      <div className="p-3 hover:bg-orange-300 dark:hover:bg-amber-800 rounded-full transition duration-200 cursor-pointer">
+        <Menu className="w-7 h-7 text-amber-600 " />
+      </div>
+
+      <div className="flex flex-col gap-3 justify-center items-center">
+        <div className="p-3 hover:bg-orange-300 dark:hover:bg-amber-800 rounded-full transition duration-200 cursor-pointer">
+          <House className="w-6 h-6 text-amber-600 " />
+        </div>
+        <div className="p-3 hover:bg-orange-300 dark:hover:bg-amber-800 rounded-full transition duration-200 cursor-pointer">
+          <FolderClosed className="w-6 h-6 text-amber-600 " />
+        </div>
+        <div className="p-3 hover:bg-orange-300 dark:hover:bg-amber-800 rounded-full transition duration-200 cursor-pointer">
+          <Archive className="w-6 h-6 text-amber-600 " />
+        </div>
+        <div className="p-3 hover:bg-orange-300 dark:hover:bg-amber-800 rounded-full transition duration-200 cursor-pointer">
+          <Trash2 className="w-6 h-6 text-amber-600 " />
+        </div>
+        <div className="p-3 hover:bg-orange-300 dark:hover:bg-amber-800 rounded-full transition duration-200 cursor-pointer">
+          <Settings className="w-6 h-6 text-amber-600 " />
+        </div>
+      </div>
     </aside>
   );
 }
